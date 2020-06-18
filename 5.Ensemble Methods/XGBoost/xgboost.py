5b5a7f68f8f28fae3dab0e434674b125e325b8f0
Jude.wang <wangyifan7836@gmail.com>
2020-06-18 12:42:29 +0800

    debug the random seed

--- 5.Ensemble Methods/XGBoost/xgboost.py
+++ 5.Ensemble Methods/XGBoost/xgboost.py
@@ -49,7 +49,7 @@ class XGBoost:
         ]
 
     def __sample(self, X):
-        random.seed(a=self.random_state)  #fixed a random seed
+        random.seed(a=self.random_state)  # fixed a random seed
         n, m = X.shape
         sub_n = int(n * self.sub_sample)
         sub_m = int(m * self.sub_feature)
@@ -58,16 +58,15 @@ class XGBoost:
         self.shuffle_sample = [random.sample([i for i in range(n)], sub_n)
                                for _ in range(self.n_estimators)]
 
-
     def fit(self, X, y):
         self.__initalize()
         self.__sample(X)
         y_pred = np.ones_like(y)
-        for i,tree in tqdm(enumerate(self.estimators)):
+        for i, tree in tqdm(enumerate(self.estimators)):
             sample_index = self.shuffle_sample[i]
             feture_index = self.shuffle_feture[i]
 
-            sub_X = X[sample_index,:][:, feture_index]
+            sub_X = X[sample_index, :][:, feture_index]
 
             sub_y = y[sample_index]
             sub_y_pred = y_pred[sample_index]
@@ -75,7 +74,7 @@ class XGBoost:
             y_and_pred = np.concatenate((sub_y, sub_y_pred), axis=1)
 
             tree.fit(sub_X, y_and_pred)
-            update_pred = tree.predict(X)
+            update_pred = tree.predict(X[:, feture_index])
 
             y_pred += np.multiply(self.learning_rate, update_pred)
 
@@ -107,7 +106,7 @@ class XGBRegressor(XGBoost):
                  sub_feature=0.8,
                  reg_lambda=1,
                  n_jobs=1,
-                 random_state=0):
+                 random_state=None):
         super().__init__(n_estimators=n_estimators,
                          max_depth=max_depth,
                          learning_rate=learning_rate,
@@ -142,7 +141,7 @@ class XGBClassifier(XGBoost):
                  sub_feature=0.8,
                  reg_lambda=1,
                  n_jobs=1,
-                 random_state=0):
+                 random_state=None):
         super().__init__(n_estimators=n_estimators,
                          max_depth=max_depth,
                          learning_rate=learning_rate,
@@ -176,14 +175,14 @@ class XGBClassifier(XGBoost):
 
 
 if __name__ == '__main__':
-    from sklearn.datasets import load_boston, make_classification,load_iris
+    from sklearn.datasets import make_classification, make_regression
     from sklearn.metrics import mean_squared_error, accuracy_score
 
-    for i in range(60, 200, 10):
-        # X, y = make_classification(n_samples=200,n_classes=3,n_informative=4,n_features=8)
-        # clf = XGBClassifier(n_estimators=i).fit(X, y)
-        # print(accuracy_score(y, clf.predict(X)))
+    for i in range(20, 200, 10):
+        X, y = make_classification(n_samples=300, n_classes=3, n_informative=4, n_features=8)
+        clf = XGBClassifier(n_estimators=i).fit(X, y)
+        print(accuracy_score(y, clf.predict(X)))
 
-        X, y = load_boston(return_X_y=True)
+        X, y = make_regression(n_samples=300)
         clf = XGBRegressor(n_estimators=i).fit(X, y)
         print(mean_squared_error(y, clf.predict(X)))
